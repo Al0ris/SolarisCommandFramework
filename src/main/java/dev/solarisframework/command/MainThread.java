b5164e66f917c3615c49e50716cb4ad7ba15bfbb
Al0ris <doublesgamez@gmail.com>
2026-08-16 17:46:48 -0400

    Added executor method to MaintThread.java

--- src/main/java/dev/solarisframework/command/MainThread.java
+++ src/main/java/dev/solarisframework/command/MainThread.java
@@ -3,7 +3,13 @@ package dev.solarisframework.command;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.concurrent.Executor;
+
 public final class MainThread {
+
+    public static Executor executor(JavaPlugin plugin) {
+        return runnable -> run(plugin, runnable);
+    }
 
     public static void run(JavaPlugin plugin, Runnable task) {
         if (Bukkit.isPrimaryThread()) {
